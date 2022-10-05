93cb8cc397368d10ef15be9325d29edbda7012ea
Olga Botvinnik <olga.botvinnik@gmail.com>
2022-10-05 06:58:19 -0700

    Start extracting exonic for splice site

--- orpheum/splicesite.py
+++ orpheum/splicesite.py
@@ -1,5 +1,14 @@
 from maxentpy import maxent_fast
+
 from orpheum.compare_kmer_content import kmerize
+
+old_chars = "ACGT"
+replace_chars = "TGCA"
+REVERSE_COMPLEMENT = str.maketrans(old_chars, replace_chars)
+
+
+def reverse_complement(sequence):
+    return sequence.translate(REVERSE_COMPLEMENT)[::-1]
 
 
 class SpliceSite:
@@ -14,9 +23,40 @@ class SpliceSite:
         return maxent_fast.score5(five_primer_9mer, matrix=self.maxent_matrix5)
 
     def score5_full_seq(self, sequence):
-        maxent_5p_scores = {kmer: self.score5_single_9mer(kmer) for kmer in kmerize(sequence, 9)}
+        maxent_5p_scores = {kmer: self.score5_single_9mer(kmer) for kmer in
+                            kmerize(sequence, 9)}
         return maxent_5p_scores
 
     def score3_full_seq(self, sequence):
-        maxent_3p_scores = {kmer: self.score3_single_23mer(kmer) for kmer in kmerize(sequence, 23)}
+        maxent_3p_scores = {kmer: self.score3_single_23mer(kmer) for kmer in
+                            kmerize(sequence, 23)}
         return maxent_3p_scores
+
+    def argmax_score5(self, sequence):
+        score5 = self.score5_full_seq(sequence)
+        argmax_kmer = max(score5, key=score5.get)
+        return argmax_kmer, score5[argmax_kmer]
+
+    def argmax_score3(self, sequence):
+        score3 = self.score3_full_seq(sequence)
+        argmax_kmer = max(score3, key=score3.get)
+        return argmax_kmer, score3[argmax_kmer]
+
+    def argmax_score5_with_rc(self, seq, seq_rc):
+        kmer5, score5 = self.argmax_score5(seq)
+        kmer5_rc, score5_rc = self.argmax_score5(seq_rc)
+        if score5 > score5_rc:
+            return kmer5, score5, 'forward'
+        else:
+            return kmer5_rc, score5_rc, 'reverse'
+
+    def argmax_score3(self, seq, seq_rc):
+        kmer3, score3 = self.argmax_score3(seq)
+        kmer3_rc, score3_rc = self.argmax_score3(seq_rc)
+        if score3 > score3_rc:
+            return kmer3, score3, 'forward'
+        else:
+            return kmer3_rc, score3_rc, 'reverse'
+
+    def extract_exonic(self, sequence):
+        seq_rc = reverse_complement(sequence)
